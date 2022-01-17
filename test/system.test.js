372fc9f5945fe91ec3a5961687c8292f5bedc72e
gdlki11 <66459117+godlikei11@users.noreply.github.com>
2022-01-17 01:12:27 +0000

    Update system.test.js

--- test/system.test.js
+++ test/system.test.js
@@ -1,18 +1,42 @@
-if(typeof document!==undefined){
     var reg = require("../reg.js");
     var login = require("../login.js")
     var app = require("../app.js")
     var network = require("../network.js")
     var Node = require("../Node.js")
 
-    describe("#Reg",function(){
+    describe("#Register",function(){
         describe("#register",function(){
-            reg.Register();
+            //reg.Register();
         })
         describe("#back",function(){
-            reg.Back();
+            //reg.Back();
         })
     })
-    //app.chessGame.drawChess();
+    describe("#Login",function(){
+        describe("#reg",function(){
 
-}
+        })
+        describe("#log",function(){
+
+        })
+    })
+    describe("#Index",function(){
+        describe("drawChessboard",function(){
+
+        })
+        describe("drawChess",function(){
+
+        })
+        describe("clearCanvas",function(){
+
+        })
+        describe("isChessThere",function(){
+
+        })
+        describe("checkWin",function(){
+
+        })
+        describe("withdraw",function(){
+
+        })
+    })
