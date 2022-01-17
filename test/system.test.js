f825db0bc507a00900c284b482504647e7ec01cf
gdlki11 <66459117+godlikei11@users.noreply.github.com>
2022-01-17 01:27:01 +0000

    Update system.test.js

--- test/system.test.js
+++ test/system.test.js
@@ -1,42 +1,21 @@
-    var reg = require("../reg.js");
-    var login = require("../login.js")
-    var app = require("../app.js")
-    var network = require("../network.js")
-    var Node = require("../Node.js")
+    let http = require('http');
+let fs = require('fs');
+let url = require('url');
+let MongoClient = require('mongodb').MongoClient;
+let WebSocket = require("ws")
+try{
+    MongoClient.connect("mongodb://localhost:27017/");
+}catch{
+    console.log("Mongodb connect fail")
+}
+try{
+    new WebSocket.Server({ port: 8081, clientTracking: true });
+}catch{
+    console.log("WebSocket connect fail")
+}
+try{
+    http.createServer().listen(8080); 
 
-    describe("#Register",function(){
-        describe("#register",function(){
-            //reg.Register();
-        })
-        describe("#back",function(){
-            //reg.Back();
-        })
-    })
-    describe("#Login",function(){
-        describe("#reg",function(){
-
-        })
-        describe("#log",function(){
-
-        })
-    })
-    describe("#Index",function(){
-        describe("drawChessboard",function(){
-
-        })
-        describe("drawChess",function(){
-
-        })
-        describe("clearCanvas",function(){
-
-        })
-        describe("isChessThere",function(){
-
-        })
-        describe("checkWin",function(){
-
-        })
-        describe("withdraw",function(){
-
-        })
-    })
+}catch{
+    console.log("Http connect fail")
+}
