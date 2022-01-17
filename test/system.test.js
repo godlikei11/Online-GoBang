37ea4f28113153c453570513ddf19537bb799898
gdlki11 <66459117+godlikei11@users.noreply.github.com>
2022-01-17 01:34:21 +0000

    Update system.test.js

--- test/system.test.js
+++ test/system.test.js
@@ -5,17 +5,21 @@ let MongoClient = require('mongodb').MongoClient;
 let WebSocket = require("ws")
 try{
     MongoClient.connect("mongodb://localhost:27017/");
+     console.log("MongoClient connect success")
 }catch{
     console.log("Mongodb connect fail")
 }
 try{
     new WebSocket.Server({ port: 8081, clientTracking: true });
+    console.log("WebSocket connect success")
 }catch{
     console.log("WebSocket connect fail")
 }
 try{
     http.createServer().listen(8080); 
-
+    console.log("Http connect success")
 }catch{
     console.log("Http connect fail")
 }
+console.log("pass");
+process.exit(port:8080)
