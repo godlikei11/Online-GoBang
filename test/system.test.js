0e9886b3b564d9d6c82d200c63e633b51b9c6470
gdlki11 <66459117+godlikei11@users.noreply.github.com>
2022-01-17 00:38:38 +0000

    Update system.test.js

--- test/system.test.js
+++ test/system.test.js
@@ -1,15 +1,18 @@
-var reg = require("../reg.js");
-var login = require("../login.js")
-var app = require("../app.js")
-var network = require("../network.js")
-var Node = require("../Node.js")
+if(typeof document!==undefined){
+    var reg = require("../reg.js");
+    var login = require("../login.js")
+    var app = require("../app.js")
+    var network = require("../network.js")
+    var Node = require("../Node.js")
 
-describe("#Reg",function(){
-    describe("#register",function(){
-        reg.Register();
+    describe("#Reg",function(){
+        describe("#register",function(){
+            reg.Register();
+        })
+        describe("#back",function(){
+            reg.Back();
+        })
     })
-    describe("#back",function(){
-        reg.Back();
-    })
-})
-//app.chessGame.drawChess();
+    //app.chessGame.drawChess();
+
+}
